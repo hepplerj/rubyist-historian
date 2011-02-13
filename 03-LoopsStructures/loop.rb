e25690068f4f676d3e999b8a1575d40ecd3bafa1
Jason Heppler <hepplerj@hotmail.com>
2011-02-13 09:03:15 -0600

    Updating program example

--- 03-LoopsStructures/loop.rb
+++ 03-LoopsStructures/loop.rb
@@ -2,18 +2,7 @@
 
 # loop.rb
 
-# ask the user for a number limit
-puts "Enter a limit: "
-
-# set the counter to 1
-num = 1
-
-# take in the user number and convert to integer (.to_i)
-limit = gets().chomp().to_i()
-
-# start the loop. Add one to the number until
-# 'num' is equal to 'limit'
-while (num <= limit)
-    puts num
-    num = num + 1
+loop do
+    print "85098357-198058903028340jj23u0280234itj3"
+    # it's just like the Matrix!
 end
